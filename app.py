6d4da51f5a0f10f9fc119cbfa388b825bad7c0ab
Manu Siddhartha <sid321axn@gmail.com>
2020-03-18 13:30:03 +0530

    keras remove

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import re
 import numpy as np
 
 # Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
